2fb94d76b45ba56315d3dfab66149a6413819959
phaxi73 <bartek1709@gmail.com>
2017-11-07 02:35:59 +0100

    SettingsActivity bar added and working

--- app/src/main/java/com/example/bartek/podzialwydatkow/SettingsActivity.java
+++ app/src/main/java/com/example/bartek/podzialwydatkow/SettingsActivity.java
@@ -5,9 +5,17 @@ import android.os.Bundle;
 
 public class SettingsActivity extends AppCompatActivity {
 
+    private android.support.v7.widget.Toolbar mToolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+
+        mToolbar = (android.support.v7.widget.Toolbar) findViewById(R.id.settings_toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle("Ustawienia Konta");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }
