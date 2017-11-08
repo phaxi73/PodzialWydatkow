188564f884a1999f9f2f4f8eb3940300ffca50cd
phaxi73 <bartek1709@gmail.com>
2017-11-08 02:00:03 +0100

    Receiving the display name from the database and showing it in SettingsActivity

--- app/src/main/java/com/example/bartek/podzialwydatkow/SettingsActivity.java
+++ app/src/main/java/com/example/bartek/podzialwydatkow/SettingsActivity.java
@@ -1,21 +1,67 @@
 package com.example.bartek.podzialwydatkow;
 
+import android.support.design.widget.TabLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class SettingsActivity extends AppCompatActivity {
 
     private android.support.v7.widget.Toolbar mToolbar;
+
+    private DatabaseReference mUserDatabase;
+    private FirebaseUser mCurrentUser;
+
+    //Android Layout
+
+    private CircleImageView mDisplayImage;
+    private TextView mName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
-
         mToolbar = (android.support.v7.widget.Toolbar) findViewById(R.id.settings_toolbar);
         setSupportActionBar(mToolbar);
         getSupportActionBar().setTitle("Ustawienia Konta");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        mCurrentUser = FirebaseAuth.getInstance().getCurrentUser();
+        mName = (TextView) findViewById(R.id.settings_display_name);
+
+        String current_uid = mCurrentUser.getUid();
+
+        mUserDatabase = FirebaseDatabase.getInstance().getReference().child("Users").child(current_uid);
+
+        mUserDatabase.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {  //Dodawanie lub otrzymywanie danych
+
+                String name= dataSnapshot.child("name").getValue().toString();
+                String image = dataSnapshot.child("image").getValue().toString();
+                String thumb_image = dataSnapshot.child("thumb_image").getValue().toString();
+
+                mName.setText(name);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) { //Obsługiwanie błędów
+
+            }
+        });
+
     }
 }
